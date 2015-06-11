426d289781a74cd641c5364a188181996a00350d
Daniel Jahn <dja@informatik.uni-kiel.de>
2015-06-11 12:07:05 +0200

    papyrus.sequence: - Adapted to KLay Layered refactoring.

--- plugins/de.cau.cs.kieler.papyrus.sequence/src/de/cau/cs/kieler/papyrus/sequence/SequenceDiagramProperties.java
+++ plugins/de.cau.cs.kieler.papyrus.sequence/src/de/cau/cs/kieler/papyrus/sequence/SequenceDiagramProperties.java
@@ -17,6 +17,7 @@ import de.cau.cs.kieler.core.kgraph.KEdge;
 import de.cau.cs.kieler.core.kgraph.KNode;
 import de.cau.cs.kieler.core.properties.IProperty;
 import de.cau.cs.kieler.core.properties.Property;
+import de.cau.cs.kieler.kiml.options.LayoutOptions;
 import de.cau.cs.kieler.klay.layered.graph.LNode;
 import de.cau.cs.kieler.papyrus.sequence.graph.SLifeline;
 import de.cau.cs.kieler.papyrus.sequence.sorter.LifelineSortingStrategy;
@@ -37,6 +38,10 @@ public final class SequenceDiagramProperties {
     /** The type of a message. */
     public static final IProperty<MessageType> MESSAGE_TYPE = new Property<MessageType>(
             "de.cau.cs.kieler.papyrus.sequence.messageType", MessageType.ASYNCHRONOUS);
+
+    /** Spacing to the border of the drawing. */
+    public static final Property<Float> BORDER_SPACING = new Property<Float>(
+            LayoutOptions.BORDER_SPACING, 12.0f, 0.0f);
 
     /** The lifeline to which an element of the SGraph belongs. */
     public static final IProperty<SLifeline> BELONGS_TO_LIFELINE = new Property<SLifeline>(
